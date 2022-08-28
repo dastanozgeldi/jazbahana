ac7beec379169ba1d542e4e4009656a7acbde370
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-28 23:52:35 +0600

    feat: add card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,9 @@
 import type { NextPage } from "next";
-import { CARD } from "../styles";
 import Page from "./layouts/Page";
-import { AiOutlineArrowRight } from "react-icons/ai";
-import Link from "next/link";
 import About from "../components/About";
 import Features from "../components/Features";
 import Contacts from "../components/Contacts";
-
-type CardProps = { title: string; children: React.ReactNode; href: string };
-
-const Card = ({ children, title, href }: CardProps) => {
-  return (
-    <div className={`${CARD} w-[300px] h-[200px]`}>
-      <h1 className="text-yellow-500 text-2xl font-semibold">{title}</h1>
-      <p>{children}</p>
-      <Link href={href}>
-        <a>
-          <AiOutlineArrowRight className="w-7 h-7" />
-        </a>
-      </Link>
-    </div>
-  );
-};
+import Card from "../components/Card";
 
 const Home: NextPage = () => {
   return (
@@ -33,16 +15,16 @@ const Home: NextPage = () => {
         </div>
 
         <div>
-          <Card href="#about" title="About Jazbahana">
+          <Card className="-ml-20" href="#about" title="About Jazbahana">
             what is Jazbahana, its importance and purposes to serve such an
             important audience.
           </Card>
 
-          <Card href="#features" title="Features">
+          <Card className="ml-20" href="#features" title="Features">
             what Jazbahana offers, the benefits of using it and many more.
           </Card>
 
-          <Card href="#contacts" title="Contacts">
+          <Card className="-ml-20" href="#contacts" title="Contacts">
             ways to connect w/ authors, leave feedback or write to support team.
           </Card>
         </div>
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,2 @@
 export const CARD =
-  "flex gap-2 flex-col items-center justify-center text-[#202020] bg-neutral-100 dark:text-neutral-100 dark:bg-[#202020] p-4 rounded-xl";
+  "my-2 flex gap-2 flex-col items-center justify-center text-[#202020] bg-neutral-100 dark:text-neutral-100 dark:bg-[#202020] p-4 rounded-xl";
