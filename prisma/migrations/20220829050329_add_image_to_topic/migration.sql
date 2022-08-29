ae19c7bbc16d1a1054c52cd257bf21b019964e7a
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-29 13:01:00 +0600

    feat(prisma): add image to topic

---
+++ prisma/migrations/20220829050329_add_image_to_topic/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Topic" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "TopicsInRooms" ADD COLUMN     "image" TEXT;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,13 @@ const prisma = new PrismaClient();
 
 async function main() {
   const firstPostId = "5c03994c-fc16-47e0-bd02-d218a370a078";
+  const topicId = "1960e2c0-c885-4b67-8b81-f8372151c063";
+
+  await prisma.topic.upsert({
+    where: { id: topicId },
+    create: { id: topicId, name: "Physics" },
+    update: {},
+  });
   await prisma.room.upsert({
     where: {
       id: firstPostId,
