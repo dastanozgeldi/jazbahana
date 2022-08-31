3364979c034edf2e3cdc2489faef77a6d2ecc6c1
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-31 16:51:59 +0600

    feat(pages): edit room page

--- src/pages/rooms/[id].tsx
+++ src/pages/rooms/[id].tsx
@@ -2,17 +2,27 @@ import { useSession } from "next-auth/react";
 import NextError from "next/error";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import Page from "../../components/layouts/Page";
 import {
   ACTION_BUTTON,
   CARD,
   DELETE_BUTTON,
+  INPUT_SELECT,
   INPUT_TEXT,
   LABEL,
 } from "../../styles";
 import { trpc } from "../../utils/trpc";
 
+type FormData = {
+  title: string;
+  description: string;
+  topicId: string;
+};
+
 const RoomViewPage = () => {
+  // Form
+  const { register, handleSubmit, reset } = useForm<FormData>();
   // Router
   const { push, query } = useRouter();
   const id = query.id as string;
@@ -21,10 +31,12 @@ const RoomViewPage = () => {
   // States
   const [title, setTitle] = useState<string | undefined>("");
   const [description, setDescription] = useState<string | undefined>("");
+  const [topicId, setTopicId] = useState<string | null | undefined>("");
   const [editing, setEditing] = useState(false);
   // tRPC
   const utils = trpc.useContext();
   const roomQuery = trpc.useQuery(["room.byId", { id }]);
+  const { data: topics } = trpc.useQuery(["topic.all"]);
   const editRoom = trpc.useMutation("room.edit", {
     async onSuccess() {
       await utils.invalidateQueries(["room.byId", { id }]);
@@ -38,9 +50,23 @@ const RoomViewPage = () => {
   });
   const { data: room } = roomQuery;
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await editRoom.mutateAsync({
+        id: room?.id || "",
+        data: {
+          title: title || "",
+          description: description || "",
+          topicId: topicId || "",
+        },
+      });
+    } catch {}
+  });
+
   useEffect(() => {
     setTitle(room?.title);
     setDescription(room?.description);
+    setTopicId(room?.topicId);
   }, []);
 
   // room fetch fail
@@ -57,7 +83,7 @@ const RoomViewPage = () => {
     return <>Loading...</>;
   }
   return (
-    <Page title={`${room.title}`} className="max-w-[60ch] mx-auto">
+    <Page title={room.title} className="max-w-[60ch] mx-auto">
       {/* Header */}
       <h1 className="text-4xl font-extrabold">{room.title}</h1>
       <p className="my-2">{room.description}</p>
@@ -89,42 +115,15 @@ const RoomViewPage = () => {
 
       {/* Edit Room */}
       <div className={`my-10 flex items-center justify-center ${CARD}`}>
-        <form
-          hidden={!editing}
-          className="w-[90%]"
-          onSubmit={async (e) => {
-            e.preventDefault();
-            /**
-             * In a real app you probably don't want to use this manually
-             * Checkout React Hook Form - it works great with tRPC
-             * @link https://react-hook-form.com/
-             */
-
-            const $title: HTMLInputElement = (e as any).target.elements.title;
-            const $description: HTMLInputElement = (e as any).target.elements
-              .description;
-            const data = {
-              title: $title.value,
-              description: $description.value,
-            };
-            try {
-              await editRoom.mutateAsync({ id, data });
-
-              $title.value = "";
-              $description.value = "";
-            } catch {}
-          }}
-        >
-          <h2 className="text-center text-3xl font-bold mb-2">Edit Post</h2>
+        <form hidden={!editing} className="w-[90%]" onSubmit={onSubmit}>
+          <h2 className="text-center text-3xl font-bold mb-2">Edit Room</h2>
           <div>
             <label className={LABEL} htmlFor="title">
               Title:
             </label>
-            <br />
             <input
               id="title"
-              name="title"
-              type="text"
+              {...register("title")}
               className={INPUT_TEXT}
               value={title}
               onChange={(e) => setTitle(e.currentTarget.value)}
@@ -136,19 +135,35 @@ const RoomViewPage = () => {
             <label className={LABEL} htmlFor="description">
               Subtitle:
             </label>
-            <br />
             <input
               id="description"
-              name="description"
-              type="text"
+              {...register("description")}
               className={INPUT_TEXT}
               value={description}
               onChange={(e) => setDescription(e.currentTarget.value)}
               disabled={editRoom.isLoading}
             />
           </div>
-
-          {/* TODO: SELECT FIELD FOR TOPIC */}
+          {/* Topics */}
+          <div className="my-4">
+            <label className={LABEL} htmlFor="topic">
+              Topic:
+            </label>
+            <select
+              {...register("topicId")}
+              id="topicId"
+              className={INPUT_SELECT}
+              onChange={(e) => setTopicId(e.currentTarget.value)}
+            >
+              <option selected>Choose a topic</option>
+              {topics &&
+                topics.map((t: any) => (
+                  <option key={t.id} value={t.id}>
+                    {t.name}
+                  </option>
+                ))}
+            </select>
+          </div>
 
           <button
             className="py-2 px-4 rounded-md text-white bg-teal-400 hover:bg-teal-500 hover:duration-500"
