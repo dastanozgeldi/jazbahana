05114284a32a7090443f9dc094884b6f26dc963f
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-09-24 21:24:32 +0600

    fix: news design

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -5,18 +5,20 @@ import { trpc } from "utils/trpc";
 const News = () => {
   const { data } = trpc.useQuery(["news.all"]);
   return (
-    <div className="max-w-[48ch] mx-auto">
+    <div className="max-w-[60ch] mx-auto">
       {/* TODO: put some filters like by popularity, date or something */}
-      <h1 className={NOTIFICATION}>
+      <h1 className={`${NOTIFICATION} m-2 p-4`}>
         Try not to miss any of the news! They might be useful to boost your
         productivity.
       </h1>
       {data?.map((item) => (
         <Link href={`/news/${item.id}`}>
-          <a className="p-4 m-2 hover:bg-gray-100 dark:hover:bg-gray-800 hover:duration-500 rounded-xl">
-            <h2 className="text-xl">{item.title}</h2>
-            <p className="text-gray-400">{item.description}</p>
-            <span className="font-semibold">{item.views} views</span>
+          <a>
+            <div className="p-4 m-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 hover:duration-500">
+              <h2 className="text-xl">{item.title}</h2>
+              <p className="text-gray-400">{item.description}</p>
+              <span className="font-semibold">{item.views} views</span>
+            </div>
           </a>
         </Link>
       ))}
