41430386a02a06eeefcc15b57b2f801c7293e972
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-11-02 09:55:53 +0600

    feat: remove extension from note

---
+++ prisma/migrations/20221101152420_remove_extension_from_note/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `extension` on the `Note` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Note" DROP COLUMN "extension";
