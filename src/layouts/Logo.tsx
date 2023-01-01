7dba580c92c56781ca6721af34fccf4a5aef2e0c
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-01-01 06:35:00 +0600

    feat: change logo & favicon

--- src/layouts/Logo.tsx
+++ src/layouts/Logo.tsx
@@ -5,10 +5,10 @@ export const Logo = () => {
   return (
     <Link
       href="/"
-      className="flex items-center px-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 hover:duration-300"
+      className="flex items-center space-x-3 p-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 hover:duration-300"
     >
-      <Image src="/logo.png" width={48} height={48} alt="logo" />
-      <span className="text-xl font-medium">Jazbahana</span>
+      <Image src="/logo.png" width={36} height={36} alt="logo" />
+      <span className="text-xl font-bold text-blue-500">Jazbahana</span>
     </Link>
   );
 };
