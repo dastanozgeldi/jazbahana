3ec900d9abbd1cf6dc7d24bbb513424ee308ed1a
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-01-08 13:13:34 +0600

    feat: edit hometask

---
+++ src/components/hometasks/EditHometask.tsx
@@ -0,0 +1,177 @@
+import type { Hometask, Topic } from "@prisma/client";
+import { Modal } from "components/common/Modal";
+import type { Session } from "next-auth";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { ACTION_BUTTON, DELETE_BUTTON, INPUT_SELECT, INPUT_TEXT } from "styles";
+import { trpc } from "utils/trpc";
+
+type EditHometaskProps = {
+  hometask?: Hometask | null;
+  topics?: Topic[] | null;
+  session?: Session | null;
+  router: any;
+};
+
+type FormData = {
+  id: string;
+  title: string;
+  content: string;
+  topicId: string;
+};
+
+export const EditHometask = ({
+  hometask,
+  topics,
+  session,
+  router,
+}: EditHometaskProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const id = router.query.id as string;
+  const userId = session?.user?.id as string;
+  const topic = topics?.find((t: Topic) => t.id === hometask?.topicId);
+  // Form
+  const { register, handleSubmit } = useForm<FormData>();
+  // tRPC
+  const utils = trpc.useContext();
+
+  const finishHometask = trpc.hometask.finish.useMutation({
+    async onSuccess() {
+      await utils.hometask.byId.invalidate({ id });
+    },
+  });
+
+  const editHometask = trpc.hometask.edit.useMutation({
+    async onSuccess() {
+      await utils.hometask.byId.invalidate({ id });
+      setIsOpen(false);
+    },
+  });
+
+  const deleteHometask = trpc.hometask.delete.useMutation({
+    async onSuccess() {
+      router.push("/workspace/hometasks");
+    },
+  });
+
+  // States
+  const [title, setTitle] = useState("");
+  const [content, setDescription] = useState("");
+  const [topicId, setTopicId] = useState("");
+
+  const onSubmit = handleSubmit(async () => {
+    try {
+      await editHometask.mutateAsync({
+        id,
+        data: {
+          title,
+          content,
+          topicId,
+        },
+      });
+    } catch {}
+  });
+
+  useEffect(() => {
+    setTitle(hometask?.title || "");
+    setDescription(hometask?.content || "");
+    setTopicId(hometask?.topicId || "");
+  }, []);
+
+  return (
+    <>
+      <div className="flex gap-2 my-2">
+        {userId === hometask?.userId && (
+          <>
+            <button
+              className={ACTION_BUTTON}
+              onClick={() => finishHometask.mutate({ id })}
+            >
+              Finish
+            </button>
+            <button
+              className={ACTION_BUTTON}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              Edit
+            </button>
+            <button
+              className={DELETE_BUTTON}
+              onClick={() => deleteHometask.mutate({ id })}
+            >
+              Delete
+            </button>
+          </>
+        )}
+      </div>
+
+      <Modal title="Edit Hometask" isOpen={isOpen} setIsOpen={setIsOpen}>
+        <form hidden={!isOpen} className="w-[90%]" onSubmit={onSubmit}>
+          <div className="my-4">
+            <label className="text-xl" htmlFor="title">
+              Title:
+            </label>
+            <input
+              id="title"
+              {...register("title")}
+              className={INPUT_TEXT}
+              value={title}
+              onChange={(e) => setTitle(e.currentTarget.value)}
+              disabled={editHometask.isLoading}
+            />
+          </div>
+          {/* Content */}
+          <div className="my-4">
+            <label className="text-xl" htmlFor="content">
+              Content:
+            </label>
+            <input
+              id="content"
+              {...register("content")}
+              className={INPUT_TEXT}
+              value={content}
+              onChange={(e) => setDescription(e.currentTarget.value)}
+              disabled={editHometask.isLoading}
+            />
+          </div>
+          {/* Topic */}
+          <div className="my-4">
+            <label className="text-xl" htmlFor="topicId">
+              Topic:
+            </label>
+            <select
+              {...register("topicId")}
+              id="topicId"
+              className={INPUT_SELECT}
+              onChange={(e) => setTopicId(e.currentTarget.value)}
+            >
+              {topic ? (
+                <option value={topic.id}>{topic.name}</option>
+              ) : (
+                <option selected>Choose a topic</option>
+              )}
+              {topics &&
+                topics.map((t: Topic) => (
+                  <option key={t.id} value={t.id}>
+                    {t.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          {/* Save */}
+          <button
+            className="py-2 px-4 rounded-md text-white bg-primary hover:bg-blue-600 hover:duration-500"
+            onClick={() => console.log(topic)}
+            disabled={editHometask.isLoading}
+          >
+            Save
+          </button>
+          {/* Error occurred */}
+          {editHometask.error && (
+            <p className="text-red-500">{editHometask.error.message}</p>
+          )}
+        </form>
+      </Modal>
+    </>
+  );
+};
--- src/pages/workspace/hometasks/[id].tsx
+++ src/pages/workspace/hometasks/[id].tsx
@@ -1,31 +1,49 @@
+import { EditHometask } from "components/hometasks/EditHometask";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import { trpc } from "utils/trpc";
 
 const HometaskView = () => {
+  const { data: session } = useSession();
   const router = useRouter();
   const id = router.query.id as string;
-  const { data, status } = trpc.hometask.byId.useQuery({ id });
+  const { data: hometask, status } = trpc.hometask.byId.useQuery({ id });
   const { status: authStatus } = useSession({
     required: true,
     onUnauthenticated() {
       router.push("/api/auth/signin");
     },
   });
+  const { data: topics } = trpc.topic.all.useQuery();
 
-  if (!data || status !== "success" || authStatus === "loading")
+  if (!hometask || status !== "success" || authStatus === "loading") {
     return "Loading or not authenticated...";
+  }
   return (
     <div className="max-w-[60ch] mx-auto">
       {/* Header */}
       <div className="border-b py-2 mb-4">
-        <h1 className="text-2xl md:text-4xl font-bold">{data.title}</h1>
-        <h2 className="text-secondary">{data.topic.name}</h2>
-        <p>{data.due?.toLocaleDateString()}</p>
+        <h1
+          className={`text-4xl font-bold ${
+            hometask.finished && "line-through"
+          }`}
+        >
+          {hometask.title}
+        </h1>
+        <h2 className="text-secondary">{hometask.topic.name}</h2>
+        <p className="text-gray-400">
+          Due: {hometask.due?.toLocaleDateString()}
+        </p>
+        <EditHometask
+          hometask={hometask}
+          router={router}
+          topics={topics}
+          session={session}
+        />
       </div>
       <ReactMarkdown className="markdown">
-        {data.content || "no data"}
+        {hometask.content || "there is no content..."}
       </ReactMarkdown>
     </div>
   );
